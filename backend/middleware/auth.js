19f29eca9ca6bb1752fc7857164e07f6fbf3925f
gdiaz5 <germandiaz555@gmail.com>
2022-09-01 20:36:32 -0400

    added authorization conroller and middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 
 
-const authUser =  async (req, res, next) {
-  const authHeader = req.headers.authrorization
+const authUser =  async (req, res, next) => {
+  const authHeader = req.headers.authorization
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     throw Error('user not authorized')
@@ -11,10 +11,17 @@ const authUser =  async (req, res, next) {
 
   const token = authHeader.split(' ')[1];
 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    //const { id, username } = decoded
+    //req.user = { id, username }
+    next()
+  } catch(error) {
+   throw new Error('error')
+  }
+
 }
 
 
 
-module.exports = {
-  authUser
-}
+module.exports = authUser
--- backend/routes/login.js
+++ backend/routes/login.js
@@ -8,10 +8,13 @@ const {login, dashboard} = require('../controllers/login');
 // Middleware
 const authUser = require('../middleware/auth');
 
+
 // GET
 router.route('/dashboard').get(authUser, dashboard);
 
+
 // POST
 router.route('/login').post(login);
+
 
 module.exports = router;
