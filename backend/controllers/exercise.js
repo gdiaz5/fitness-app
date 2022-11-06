40733d1fb44c86ad143dfe1f64127c57d5ac6888
gdiaz5 <germandiaz555@gmail.com>
2022-11-05 20:23:46 -0400

    added login in verification

--- backend/controllers/exercise.js
+++ backend/controllers/exercise.js
@@ -5,10 +5,11 @@ const workoutCard = require('../models/Exercise/workoutCard')
 
 // Adds new workout
 const createExercise = async (req, res) => {
-  // req.body.createdBy = req.user.userId
-  // const exercise = await Exercise.create(req.body);
-  // res.status(200).json({ exercise })
-  res.json(req.user)
+  req.body.createdBy = req.user.userId
+  const exercise = await Exercise.create(req.body);
+  console.log(exercise.createdBy)
+  res.status(200).json({ exercise })
+ // res.json(req.user)
 }
 
 
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,7 +10,7 @@ const authUser =  async (req, res, next) => {
   }
 
   const token = authHeader.split(' ')[1];
-  console.log(token)
+
 
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
