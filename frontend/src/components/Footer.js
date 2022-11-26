f83fcbae4123cbe488f54efe7f900c039df9c4c7
gdiaz5 <germandiaz555@gmail.com>
2022-11-25 19:20:52 -0500

    added styling to the footer and established github a-tag link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import '../styles/_footer.scss'
+import { FaInstagram, FaGithub, FaYoutube } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <h2>swole buddy</h2>
+      <div className='footer_social'>
+        <a href='/'>
+          <FaInstagram  color='white' size={25}/>
+        </a>
+        <a href='https://github.com/gdiaz5'>
+          <FaGithub  color='white' size={25}/>
+        </a>
+        <a href='/'>
+          <FaYoutube  color='white' size={25}/>
+        </a>
+      </div>
+      <div className='footer_link'>
+        <a href='/'>
+          <p>Home</p>
+        </a>
+        <a href='/'>
+          <p>Sign Up</p>
+        </a>
+        <a href='/'>
+          <p>About Us</p>
+        </a>
+        <a href='/'>
+          <p>Blog</p>
+        </a>
+      </div>
+    </div>
   )
 }
 
