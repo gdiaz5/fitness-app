0e75b2868ac6d8c478bd77f8deb65ae6a8becb9a
gdiaz5 <germandiaz555@gmail.com>
2022-11-27 17:23:26 -0500

    updated login button and added email and password state

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,22 +1,39 @@
 import React from 'react'
 import '../styles/_register.scss'
 import logo from '../assests/SB-removebg-preview.png'
+import { useState } from 'react'
 
 function Register() {
+
+  // State
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+    setEmail('')
+    setPassword('')
+  }
+
+
+
   return (
     <div className='register'>
       <div className='register_form'>
         <img src={logo} alt='' />
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Swole Buddy</h1>
         <div>
           <label>
-            <input type='text' placeholder='Email'/>
+            <input type='text' value={email} placeholder='Email'
+            onChange={(e) => setEmail(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
-            <input type='text' placeholder='Password'/>
+            <input type='password' value={password} placeholder='Password'
+            onChange={(e) => setPassword(e.target.value)}/>
           </label>
         </div>
          <input className='btn' type='submit' value='Login'/>
